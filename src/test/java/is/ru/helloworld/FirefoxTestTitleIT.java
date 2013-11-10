2d10888b706458a26feaf9e6025cfd769368ee48
Skúli Arnlaugsson <skuli@gagnavarslan.is>
2013-11-10 15:11:31 +0000

    Added an initial test running Firefox

---
+++ src/test/java/is/ru/helloworld/FirefoxTestTitleIT.java
@@ -0,0 +1,48 @@
+package is.ru.helloworld;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class FirefoxTestTitleIT {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost:4567";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void titleShouldBeHelloWorld() throws Exception {
+    driver.get(baseUrl);
+    assertEquals("Hello world", driver.getTitle());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+}
