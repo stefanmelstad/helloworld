b2fd2575ebf243b68eaf6800519ae30f8c0247a1
Skúli Arnlaugsson <skuli@gagnavarslan.is>
2013-11-10 12:39:51 +0000

    Added selenium test and command to run
    And updated the README about where to get ChromeDriver etc.

---
+++ README.md
@@ -0,0 +1,7 @@
+helloworld
+===
+
+Selenium tests
+---
+
+We are using ChromeDriver in our demo - you'll need to [download it](http://chromedriver.storage.googleapis.com/index.html) and make sure it's accessible from the terminal (or command line) - see [documentation](http://code.google.com/p/selenium/wiki/ChromeDriver).
---
+++ src/test/java/is/ru/helloworld/HelloWorldIT.java
@@ -0,0 +1,54 @@
+package is.ru.helloworld;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class HelloWorldIT {
+    System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
+    private static ChromeDriverService service;
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void createAndStartService() throws IOException {
+     service = new ChromeDriverService.Builder()
+         //.usingDriverExecutable(new File("chromedriver"))
+         .usingAnyFreePort()
+         .build();
+     service.start();
+    }
+
+     @AfterClass
+     public static void createAndStopService() {
+         service.stop();
+     }
+
+     @Before
+     public void createDriver() {
+         driver = new RemoteWebDriver(service.getUrl(), DesiredCapabilities.chrome());
+     }
+
+     @After
+     public void quitDriver() {
+         driver.quit();
+     }
+
+     @Test
+     public void testTitle() throws Exception {
+      driver.get("http://localhost:4567/");
+      assertEquals("Hello world", driver.getTitle());
+    }
+}
