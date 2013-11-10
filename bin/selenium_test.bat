53626cc409394456ada7e545ab9d36785f525252
Skúli Arnlaugsson <skuli@gagnavarslan.is>
2013-11-10 13:43:29 +0000

    Only run selenium tests if test_compile finishes

--- bin/selenium_test.bat
+++ bin/selenium_test.bat
@@ -3,5 +3,4 @@ rem ***********************************************************************
 rem This bat file runs Selenium tests (uses failsafe plugin).
 rem ***********************************************************************
 
-call mvn test-compile
-call mvn failsafe:integration-test
+call mvn test-compile failsafe:integration-test
